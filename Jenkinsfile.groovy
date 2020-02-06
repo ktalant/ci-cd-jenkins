3f606026f5dc81cefa2320f92a6cd77ad1933a80
ktalant <uchkunfan@gmail.com>
2020-02-05 20:17:14 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,17 +2,21 @@ node {
     properties([pipelineTriggers([cron('* * * * *')])])
 
 
-    stage("Stage1"){
+    stage("Pull Repo"){
         echo "Hello, World!!!"
     }
-    stage("Stage2"){
+    stage("Deploy to Dev"){
         echo "Hello, World!!!"
     }
-    stage("Stage3"){
+    stage("Wait"){
+        input 'Deploy to QA?'
+    }
+    stage("Deploy to QA"){
         echo "Hello, World!!!"
     }
     stage("Send notifications to Slack"){
         echo "Hello, World!!!"
         slackSend channel: 'nagios_alerts', message: 'Completed!'
+        mail bcc: 'uchkunfan@gmail.com', body: 'completed task', cc: 'uchkunfan@gmail.com', from: '', replyTo: '', subject: 'completed', to: 'uchkunfan@gmail.com'
     }
 }
