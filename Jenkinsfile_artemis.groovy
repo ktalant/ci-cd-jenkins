0eebf55c3e0dcd5611f0d968d984e03c3ad83b84
ktalant <uchkunfan@gmail.com>
2020-02-10 16:40:19 -0500

    updated

--- Jenkinsfile_artemis.groovy
+++ Jenkinsfile_artemis.groovy
@@ -1,20 +1,14 @@
 node {
     properties([
-        //dicard old builds parameter
-        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', 
-        numToKeepStr: '4'))],
-        
-        
-        parameters([
-            //choice parameter for app_version
-            choice(choices: ['version/0.1', 'version/0.2', 'version/0.3', 'version/0.4', 'version/0.5'], 
-            description: 'choose ARTEMIS_APP_VERSION to be deployed', name: 'ARTEMIS_VERSION'),
-
-            //choice parameter for app_envrionment
-            choice(choices: ['dev1.talantzon.com', 'qa1.talantzon.com', 'stage1.talantzon.com', 'prod1.talantzon.com'], 
-            description: 'Choose Environment ', name: 'ARTEMIS_ENV')
+            buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', 
+            daysToKeepStr: '', numToKeepStr: '4')), 
             
-            ]))
+            parameters([
+                choice(choices: ['version/0.1', 'version/0.2', 'version/0.3', 'version/0.4', 'version/0.5', 'version/0.6'], 
+                description: 'Choose app version to be deployed', name: 'ARTEMIS_VERSION'), 
+                
+                choice(choices: ['dev1.talantzon.com', 'qa1.talantzon.com', 'stage1.talantzon.com', 'prod1.talantzon.com'], 
+                description: 'Choose environment ', name: 'ARTEMIS_ENV')])])
 
 
     stage("Pull repo"){
