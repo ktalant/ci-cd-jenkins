65d6af427016615dfc9c5fb01a42f44b5c6b5bab
ktalant <uchkunfan@gmail.com>
2020-02-10 16:42:03 -0500

    updated

--- Jenkinsfile_artemis.groovy
+++ Jenkinsfile_artemis.groovy
@@ -1,12 +1,15 @@
 node {
     properties([
+            //discard old builds set to 4
             buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', 
             daysToKeepStr: '', numToKeepStr: '4')), 
             
             parameters([
+                //choice parameter for app version
                 choice(choices: ['version/0.1', 'version/0.2', 'version/0.3', 'version/0.4', 'version/0.5', 'version/0.6'], 
                 description: 'Choose app version to be deployed', name: 'ARTEMIS_VERSION'), 
                 
+                //choice parameter for app environment
                 choice(choices: ['dev1.talantzon.com', 'qa1.talantzon.com', 'stage1.talantzon.com', 'prod1.talantzon.com'], 
                 description: 'Choose environment ', name: 'ARTEMIS_ENV')])])
 
