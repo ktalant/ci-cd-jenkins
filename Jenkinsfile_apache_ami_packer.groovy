10e881243a6ea4b011c5dfa788a5cecae5c0e683
ktalant <uchkunfan@gmail.com>
2020-02-09 22:25:50 -0500

    log rotation  has been added

--- Jenkinsfile_apache_ami_packer.groovy
+++ Jenkinsfile_apache_ami_packer.groovy
@@ -1,16 +1,21 @@
 node {
     properties([
+        //Number or builds to be discarded
         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', 
         daysToKeepStr: '', numToKeepStr: '4')),
-        
+
         parameters([
+        // region choice parameter
         choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], 
         description: 'Choose the region for AMI to be created', 
         name: 'AMI_REGION'),
 
+        // instance_type string parameter
         string(defaultValue: 't2.micro', 
         description: 'Enter proper instance type, e.g (t2.medium, m2.large etc)', 
-        name: 'INSTANCE_TYPE', trim: false)])])
+        name: 'INSTANCE_TYPE', trim: false)])
+        
+        ])
 
 
     stage("Pull repo"){
